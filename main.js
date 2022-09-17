5568fdbc28f90dcc7886da6b4d0c1842f03ffb38
taikhumb <githubcoder123@gmail.com>
2022-09-17 20:27:19 +0530

    saving changes to github

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ function change(){
     if (pic.src.match("download.jpg"))
     {
         pic.src="download (2).jpg";
+        pic.style.height = "20%";
     }
     else
     {
         pic.src =  "download.jpg";
+        pic.style.height = "30%";
+    };
+    var mq = window.matchMedia( "(max-width: 999px)" );
+    if (mq.matches){    
+        
     }
+    else
+    {
+      pic.style.height = "70%";
+    }
+    
+
         
 
     
